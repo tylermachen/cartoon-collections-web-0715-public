8422f02005ce59ebefb53d311e2b00d76e8b3098
Tyler Machen <tylermachen@gmail.com>
2015-07-31 14:57:04 -0400

    complete dwarf methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each.with_index(1) { |dwarf, num| puts "#{num}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize << "!" }
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
+  # valid cheese types
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find { |i| cheese_types.include?(i) }
 end
